68194e202a5e991d04ecc2a07a4dc054c196d2e7
Eefje van Asperen <eefjevanasperen@hotmail.com>
2024-12-30 11:11:38 +0100

    Created profile page and profile stats component

---
+++ app/(blog-pages)/profile/page.jsx
@@ -0,0 +1,9 @@
+import ProfileStats from "@/components/ProfileStats"
+
+export default function Profile() {
+    return (
+        <div className="flex w-screen justify-center mt-[20px]">
+            <ProfileStats></ProfileStats>
+        </div>
+    );
+  }
