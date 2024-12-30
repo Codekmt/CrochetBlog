62bab510cf837c333a0c577d1320ac887f743485
Julia <julia.bongon@gmail.com>
2024-12-30 11:55:28 +0100

    Make page to create Help post

---
+++ app/(blog-pages)/CreateHelpPost/page.tsx
@@ -0,0 +1,12 @@
+import CreateHelpPost from "@/components/CreateHelpPost";
+
+
+const HelpPostPage = () => {
+  return (
+    <div>
+      <CreateHelpPost />
+    </div>
+  );
+};
+
+export default HelpPostPage;
