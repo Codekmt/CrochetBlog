688610fc096e40c696518683c9ebfcb233029398
Eefje van Asperen <eefjevanasperen@hotmail.com>
2024-12-30 16:17:31 +0100

    Added responsiveness to profile page

--- app/(blog-pages)/profile/page.jsx
+++ app/(blog-pages)/profile/page.jsx
@@ -1,9 +1,19 @@
+import PatternPost from "@/components/PatternPost";
 import ProfileStats from "@/components/ProfileStats"
 
 export default function Profile() {
     return (
-        <div className="flex w-screen justify-center mt-[20px]">
-            <ProfileStats></ProfileStats>
+        <>
+        <div className="mt-[20px]">
+            <div className="flex flex-col lg:grid lg:grid-cols-3">
+                <div className="lg:col-start-1 lg:col-end-2 lg:row-start-1 flex lg:items-start justify-center">
+                <ProfileStats />
+                </div>
+                <div className="lg:col-start-2 lg:col-end-3 lg:row-start-1 flex lg:items-start flex items-center justify-center">
+                <PatternPost />
+                </div>
+            </div>
         </div>
+        </>
     );
   }
