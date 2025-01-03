c5c365a7ae709a7e524a2efb886046449f686730
Eefje van Asperen <eefjevanasperen@hotmail.com>
2025-01-03 12:18:05 +0100

    Created fetch request for all posts

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,9 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
-// import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
