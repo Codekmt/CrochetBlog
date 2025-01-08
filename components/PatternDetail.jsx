aa3f60cb550474d7b78964ee3a2ef4cd106c3385
Eefje van Asperen <eefjevanasperen@hotmail.com>
2025-01-08 11:57:06 +0100

    Add dummy data to pattern post

--- components/PatternDetail.jsx
+++ components/PatternDetail.jsx
@@ -15,10 +15,18 @@ export default function PatternDetail() {
 
     Enjoy the pattern!`
 
+    const pictures = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"];
+
+    const coverPicture = "01.jpg";
+
     return (
         <div className="w-[400px] sm:w-[500px]">
             <div className="flex items-center gap-[10px]">
-                <div className="bg-gray-200 w-[50px] h-[50px] rounded-full"></div>
+                <div className="bg-gray-200 w-[50px] h-[50px] rounded-full flex items-center">
+                    <span role="img" aria-label="Profile" className="text-2xl ml-[10px]">
+                    👤
+                    </span>
+                </div>
                 <p>Username123</p>
                 <div className="bg-gray-200 pl-[10px] pr-[10px] ml-auto">{tag}</div>
             </div>
@@ -27,18 +35,19 @@ export default function PatternDetail() {
                 <p className="italic">short description in which I explain what kind of patterns this is and what you can use it for.</p>
             </div>
             <div className="ml-[60px] mt-[10px] w-[350px] h-[350px] sm:w-[400px] sm:h-[400px]">
-                <div className="bg-gray-200 w-full h-full"></div>
+                <img src={coverPicture} className="w-full h-full object-cover"></img>
             </div>
             <div className="ml-[60px]">
                 <p className="whitespace-pre-line">
                     {description}
                 </p>
             </div>
-            <div className="flex flex-col ml-[60px] mt-[10px] w-[350px] h-auto sm:w-[400px] sm:h-auto gap-[10px]">
-                <div className="bg-gray-200 w-full h-[400px]"></div>
-                <div className="bg-gray-200 w-full h-[400px]"></div>
-                <div className="bg-gray-200 w-full h-[400px]"></div>
-                <div className="bg-gray-200 w-full h-[400px]"></div>
+            <div>
+                {pictures.map((picture, index) => (
+                    <div className="flex flex-col ml-[60px] mt-[10px] w-[350px] h-auto sm:w-[400px] sm:h-auto gap-[10px]" key={index}>
+                        <img index={index} src={`/${picture}`} className="w-full h-full object-cover"></img>
+                    </div>
+                ))}
             </div>
             <SocialBar></SocialBar>
         </div>
--- components/PatternPost.jsx
+++ components/PatternPost.jsx
@@ -5,11 +5,16 @@ import SocialBar from "./socialBar";
 export default function PatternPost() {
 
     const tag = "tag";
+    const picture = "01.jpg"
 
     return (
         <div className="w-[400px] sm:w-[500px]">
             <div className="flex items-center gap-[10px]">
-                <div className="bg-gray-200 w-[50px] h-[50px] rounded-full"></div>
+                <div className="bg-gray-200 w-[50px] h-[50px] rounded-full flex items-center">
+                    <span role="img" aria-label="Profile" className="text-2xl ml-[10px]">
+                    👤
+                    </span>
+                </div>
                 <p>Username123</p>
                 <div className="bg-gray-200 pl-[10px] pr-[10px] ml-auto">{tag}</div>
             </div>
@@ -18,7 +23,9 @@ export default function PatternPost() {
                 <Link href="/Pattern" className="text-gray-500 mb-[10px]">View full pattern</Link>
             </div>
             <div className="ml-[60px] mt-[10px] w-[350px] h-[350px] sm:w-[400px] sm:h-[400px]">
-                <div className="bg-gray-200 w-full h-full"></div>
+                <div className="w-full h-full">
+                    <img src={picture} className="w-full h-full object-cover"></img>
+                </div>
             </div>
             <SocialBar></SocialBar>
         </div>
