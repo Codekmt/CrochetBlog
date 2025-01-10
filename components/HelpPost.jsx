546ddf7cefc6e796ed00c1730c3cc1c240c77bdc
Rana <39658207+ranadealmeida@users.noreply.github.com>
2025-01-10 11:27:22 +0100

    Merge conflict

--- components/HelpPost.jsx
+++ components/HelpPost.jsx
@@ -62,13 +62,8 @@ export default function HelpPost() {
 
     return (
         <div className="flex flex-col gap-[20px]">
-<<<<<<< HEAD
-            {posts.map((post) => (
-                <div className="w-[400px] sm:w-[500px]" key={post.id}>
-=======
             {posts.map((post, index) => (
                 <div className="w-[400px] sm:w-[500px]" key={index}>
->>>>>>> 56ab5abcd2bc0801c869e6c376cda0f3741d8887
                     <div className="flex items-center gap-[10px]">
                         <div className="bg-gray-200 w-[50px] h-[50px] rounded-full flex items-center">
                             <span role="img" aria-label="Profile" className="text-2xl ml-[10px]">
