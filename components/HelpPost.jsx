33f350ced3dc5b20b0700ad99d80f811895e3f5a
Eefje van Asperen <eefjevanasperen@hotmail.com>
2025-01-10 10:47:22 +0100

    Add all posts to landing page

--- components/HelpPost.jsx
+++ components/HelpPost.jsx
@@ -92,7 +92,7 @@ export default function HelpPost() {
     return (
         <div className="flex flex-col gap-[20px]">
             {posts.map((post, index) => (
-                <div className="w-[400px] sm:w-[500px]">
+                <div className="w-[400px] sm:w-[500px]" key={index}>
                     <div className="flex items-center gap-[10px]">
                         <div className="bg-gray-200 w-[50px] h-[50px] rounded-full flex items-center">
                             <span role="img" aria-label="Profile" className="text-2xl ml-[10px]">
